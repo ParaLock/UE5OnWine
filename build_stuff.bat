9792c1566d66b59933b64420f117a19003e9065e
ParaLock <nathanael.mercaldo@gmail.com>
2023-05-11 16:35:43 +0000

    Delete build_stuff.bat

--- build_stuff.bat
+++
@@ -1,3 +0,0 @@
-call Engine\Build\BatchFiles\Build.bat ShaderCompileWorker Win64 Development -waitmutex
-call Engine\Build\BatchFiles\Build.bat UnrealEditor Win64 Development -waitmutex
-call Engine\Build\BatchFiles\Build.bat CitySampleEditor Development Win64 "C:\users\linuxdev\Documents\Unreal Projects\CitySample\CitySample2\CitySample2.uproject" -Progress -waitmutex
